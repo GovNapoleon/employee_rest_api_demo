c33899729eac839484b43158ba9e5664c4f17328
Napoleon K. <95312667+GovNapoleon@users.noreply.github.com>
2025-12-25 13:21:29 -0600

    rails db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.2].define(version: 2025_12_25_191955) do
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.boolean "haspassport"
+    t.integer "salary"
+    t.date "birthdate"
+    t.date "hiredate"
+    t.string "gender"
+    t.string "notes"
+    t.string "email"
+    t.string "phone"
+    t.integer "country_id", null: false
+    t.integer "department_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["country_id"], name: "index_employees_on_country_id"
+    t.index ["department_id"], name: "index_employees_on_department_id"
+  end
+
+  add_foreign_key "employees", "countries"
+  add_foreign_key "employees", "departments"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,52 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+unless Department.blank?
+  Department.create!(name: "Networking")
+  Department.create!(name: "Dispatch")
+  Department.create!(name: "Marketting")
+  Department.create!(name: "Shipping")
+end
+
+
+unless Country.blank?
+  Country.create!(name: "UK")
+  Country.create!(name: "Spain")
+  Country.create!(name: "Brazil")
+  Country.create!(name: "Italy")
+end
+
+unless Employee.blank?
+  Employee.create!(firstname: "Connors",lastname:"McGregor",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Connors@gmail.com',phone:'12345',country_id:1,department_id:1)
+
+  Employee.create!(firstname: "Chad",lastname:"Mendis",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Chad@gmail.com',phone:'12345',country_id:1,department_id:1)
+
+  Employee.create!(firstname: "Nate",lastname:"Diaz",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Nate@gmail.com',phone:'12345',country_id:1,department_id:1)
+
+  Employee.create!(firstname: "Nick",lastname:"Diaz",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Nick@gmail.com',phone:'12345',country_id:2,department_id:2)
+
+  Employee.create!(firstname: "Anthony",lastname:"Joshua",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Anthony@gmail.com',phone:'12345',country_id:2,department_id:2)
+
+  Employee.create!(firstname: "Michael",lastname:"Johnson",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Michael@gmail.com',phone:'12345',country_id:3,department_id:3)
+
+  Employee.create!(firstname: "Israel",lastname:"Adesanya",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Adesanya@gmail.com',phone:'12345',country_id:3,department_id:3)
+
+  Employee.create!(firstname: "Michael",lastname:"Chang",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Chang@gmail.com',phone:'12345',country_id:4,department_id:4)
+
+  Employee.create!(firstname: "Novak",lastname:"Djokovic",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Novak@gmail.com',phone:'12345',country_id:4,department_id:4)
+
+  Employee.create!(firstname: "Pete",lastname:"Samprass",haspassport:true,salary:5000,birthdate:'2000-1-1',
+                   hiredate:'2020-1-1',gender:'Male',notes:'Notorious Guy!',email:'Pete@gmail.com',phone:'12345',country_id:4,department_id:4)
+
+
+end
