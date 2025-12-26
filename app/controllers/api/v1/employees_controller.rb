623bdb852b93d1eceb0fc16ade66f5f211769623
Napoleon K. <95312667+GovNapoleon@users.noreply.github.com>
2025-12-26 08:51:06 -0600

    Implement update and show

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,6 +1,49 @@
 class Api::V1::EmployeesController < ApplicationController
+
+  before_action :set_employee, only: [:show, :update, :destroy]
   def index
     @employees = Employee.includes(:department, :country)
     render json: @employees, status: :ok
+  end
+
+  def show
+    render json: @employee, status: :ok
+  end
+
+  def update
+    if @employee.update(employees_params)
+      render json: @employee
+    else
+      render json: @employee.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+  def set_employee
+    begin
+      @employee = Employee.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Employee not found' }, status: :not_found
+    end
+  end
+
+  def employees_params
+    params.require(:employee).permit(
+      :firtname, :lastname,
+      :haspassport,
+      :salary,
+      :birthdate,
+      :hiredate,
+      :gender,
+      :notes,
+      :email,
+      :phone,
+      :country_id,
+      :department_id
+    )
   end
 end
